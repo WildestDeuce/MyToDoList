60143caabbdb7cecaae6a58e12f053159d212665
WildestDeuce <48850844+WildestDeuce@users.noreply.github.com>
2021-02-13 22:46:35 -0500

    creatged handleFilter constant

--- src/App.js
+++ src/App.js
@@ -10,12 +10,21 @@ import ToDoList from './ToDoList';
 function App() {
 
   const [toDoList, setToDoList] = useState(data);
+
   const handleToggle = (id) => {
     let mapped = toDoList.map(task => {
       return task.id == id ? { ...task, complete: !task.complete } : { ...task };
     });
     setToDoList(mapped);
   }
+
+  const handleFilter = () => {
+    let filtered = toDoList.filter(task => {
+      return !task.complete;
+    });
+    setToDoList(filtered);
+  }
+
   return (
     <div className="App">
       <Header />
